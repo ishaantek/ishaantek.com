499ecc0b0b326a81e8b9ed9086480b86a46b4465
Ishaan Garg <ishaan@ishaantek.com>
2022-07-16 13:57:19 -0400

    test

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/My-version.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/kda.mp4",
+    },
+];
